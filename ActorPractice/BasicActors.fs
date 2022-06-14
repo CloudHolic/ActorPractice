b0721d0274eba22b361ce84404d871818b641d48
CloudHolic <bluegkstjr@kaist.ac.kr>
2022-06-14 16:18:39 +0900

    Add project files.

---
+++ ActorPractice/BasicActors.fs
@@ -0,0 +1,45 @@
+﻿module BasicActor
+
+type Message = string * AsyncReplyChannel<string>
+
+let firstAgent =
+    MailboxProcessor.Start (fun inbox ->
+        async {
+            while true do
+                let! msg = inbox.Receive()
+                printfn "got message '%s'" msg
+        })
+
+let replyAgent =
+    MailboxProcessor<Message>.Start (fun inbox ->
+        let rec loop () =
+            async { 
+                let! (message, replyChannel) = inbox.Receive()
+                replyChannel.Reply($"Received message: {message}")
+                do! loop ()
+            }
+        loop ())
+
+let scanAgent =
+    MailboxProcessor.Start (fun inbox ->
+        async {
+            while true do
+                do! inbox.Scan (fun hello ->
+                    match hello with
+                    | "Hello!" -> 
+                        Some(async { printfn "This is a hello message!"})
+                    | _ -> None)
+                let! msg = inbox.Receive()
+                printfn "Got message '%s'" msg})
+
+
+["1"; "2"; "3"; "4"; "5"; "6"; "7"; "8"; "9"; "10";
+"Hello!"; "Hello!"; "Hello!"; "Hello!"; "Hello!"; "Hello!"; 
+"Hello!"; "Hello!"; "Hello!"; "Hello!"]
+|> List.map scanAgent.Post |> ignore
+
+firstAgent.Post "Hello!"
+
+let receive = replyAgent.PostAndReply (fun rc -> "Hello", rc) // "Received message: Hello"
+
+printfn "%s" receive
---
+++ ActorPractice/ExampleActor.fs
@@ -0,0 +1,52 @@
+﻿module ExampleActor
+
+open System.Collections.Generic
+
+type CoordinatorMessage =
+    | Job of int
+    | Ready
+    | RequestJob of AsyncReplyChannel<int>
+
+let Coordinator =
+    MailboxProcessor<CoordinatorMessage>.Start (fun inbox ->
+        let queue = new Queue<int> ()
+        let mutable count = 0
+        let rec loop () =
+            async {
+                while count < 4 do
+                    do! inbox.Scan (function
+                        | Ready -> Some (async { count <- count + 1 })
+                        | Job _ -> None
+                        | RequestJob _ -> None)
+                let! message = inbox.Receive ()
+                match message with
+                | Job length -> queue.Enqueue length
+                | RequestJob replyChannel -> replyChannel.Reply <| queue.Dequeue ()
+                | Ready -> ()
+                
+                return! loop ()}
+        loop ())
+
+let Worker () =
+    MailboxProcessor<bool>.Start (fun inbox ->
+        Coordinator.Post Ready
+        let rec loop () =
+            async { 
+                let! length = Coordinator.PostAndAsyncReply RequestJob
+                do! Async.Sleep length
+                return! loop ()}
+
+        loop())
+
+
+// Create fake jobs
+let rand = new System.Random(941345563)
+Seq.init 500 (fun _ -> rand.Next(1, 101))
+|> Seq.iter (fun r -> Coordinator.Post(Job r))
+
+
+// Create worker agents
+let JobAgent1 = Worker ()
+let JobAgent2 = Worker ()
+let JobAgent3 = Worker ()
+let JobAgent4 = Worker ()
